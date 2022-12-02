20ba8935e2cd0d1e48b94fd09eb13b12e9ffa54b
Andreas Uschold <andreas.uschold@iits-consulting.de>
2022-12-02 12:55:57 +0100

    d02.2

--- src/main/kotlin/d02/d02.kt
+++ src/main/kotlin/d02/d02.kt
@@ -7,13 +7,14 @@ fun main() {
 
     val totalPoints = file.readLines()
         .map { parseRoundInput(it) }
+        .map { it.first to shapeForRequiredOutcome(it.first, it.second) }
         .sumOf { calculatePointsForSingleRound(it.first, it.second) }
 
     println(totalPoints)
 }
 
-private fun parseRoundInput(line: String): Pair<Shape, Shape> {
-    val (opponentSymbol, ownSymbol) = line.split(" ")
+private fun parseRoundInput(line: String): Pair<Shape, Outcome> {
+    val (opponentSymbol, outcomeSymbol) = line.split(" ")
     val opponentShape = when (opponentSymbol) {
         "A" -> Shape.ROCK
         "B" -> Shape.PAPER
@@ -21,14 +22,14 @@ private fun parseRoundInput(line: String): Pair<Shape, Shape> {
         else -> error("Unexpected input symbol $opponentSymbol")
     }
 
-    val ownShape = when (ownSymbol) {
-        "X" -> Shape.ROCK
-        "Y" -> Shape.PAPER
-        "Z" -> Shape.SCISSORS
-        else -> error("Unexpected input symbol $opponentSymbol")
+    val requiredOutcome = when (outcomeSymbol) {
+        "X" -> Outcome.LOSE
+        "Y" -> Outcome.DRAW
+        "Z" -> Outcome.WIN
+        else -> error("Unexpected input symbol $outcomeSymbol")
     }
 
-    return Pair(opponentShape, ownShape)
+    return Pair(opponentShape, requiredOutcome)
 }
 
 private fun calculatePointsForSingleRound(opponentShape: Shape, ownShape: Shape) =
@@ -38,15 +39,21 @@ private fun roundOutcome(opponentShape: Shape, ownShape: Shape): Outcome {
     return when {
         opponentShape == ownShape -> Outcome.DRAW
         ownShape.beats(opponentShape) -> Outcome.WIN
-        else -> Outcome.LOSS
+        else -> Outcome.LOSE
     }
 }
+
+private fun shapeForRequiredOutcome(opponentShape: Shape, requiredOutcome: Outcome): Shape =
+    when (requiredOutcome) {
+        Outcome.DRAW -> opponentShape
+        Outcome.WIN -> Shape.values().single { it.beats(opponentShape) }
+        Outcome.LOSE -> Shape.values().single { opponentShape.beats(it) }
+    }
 
 enum class Shape(val points: Int) {
     ROCK(1),
     PAPER(2),
     SCISSORS(3);
-
 }
 
 private fun Shape.beats(other: Shape): Boolean = when (this) {
@@ -58,6 +65,6 @@ private fun Shape.beats(other: Shape): Boolean = when (this) {
 enum class Outcome(val points: Int) {
     WIN(6),
     DRAW(3),
-    LOSS(0);
+    LOSE(0);
 
 }
