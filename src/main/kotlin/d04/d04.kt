6258fa6ebd63858b7690b5316f96429c6b727592
Andreas Uschold <andreas.uschold@iits-consulting.de>
2022-12-04 09:47:32 +0100

    d04.2

--- src/main/kotlin/d04/d04.kt
+++ src/main/kotlin/d04/d04.kt
@@ -10,10 +10,19 @@ fun main() {
         .count { isFullyRedundantSectionAssignment(it) }
 
     println(numberOfRedundantAssignments)
+
+    val numberOfOverlappingAssignments = file.readLines()
+        .map { parseAssignedSections(it) }
+        .count { isOverlappingSectionAssignment(it) }
+
+    println(numberOfOverlappingAssignments)
 }
 
 private fun isFullyRedundantSectionAssignment(assignment: Pair<IntRange, IntRange>) =
     assignment.first.contains(assignment.second) || assignment.second.contains(assignment.first)
+
+private fun isOverlappingSectionAssignment(assignment: Pair<IntRange, IntRange>) =
+    assignment.first.overlaps(assignment.second)
 
 private fun parseAssignedSections(input: String): Pair<IntRange, IntRange> {
     val (sectionsFirstElf, sectionsSecondElf) = input.split(",")
@@ -27,3 +36,7 @@ private fun parseSectionRange(input: String): IntRange {
 
 private fun IntRange.contains(other: IntRange) =
     this.contains(other.first) && this.contains(other.last)
+
+private fun IntRange.overlaps(other: IntRange) =
+    this.contains(other.first) || this.contains(other.last) ||
+            other.contains(this.first) || other.contains(this.last)
